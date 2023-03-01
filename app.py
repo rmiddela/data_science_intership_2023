0d1d952baa7e2394b8dca0c278986c7a237bbf49
MIDDELA RAMESH <105881615+rmiddela@users.noreply.github.com>
2023-03-01 20:32:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("My Portfolio")
 st.header("Middela Ramesh")
 st.header("Datascience Intern @ Innomatics Research Labs")
-st.subheader("Dedicated and detail-oriented early career Data Science and Machine Learning enthusist '\n' who is determined for Data Analyst and Data Scientist Roles ")
+st.subheader("Dedicated and detail-oriented early career Data Science and Machine Learning enthusist who is determined for Data Analyst and Data Scientist Roles ")
